652b77e66937875501001f3a47f28a01b8eb9413
Matthias Hamacher <hamachermatthias12@gmail.com>
2023-04-01 23:41:19 +0200

    fixed function attribute issue

--- tests/decorators_tests.py
+++ tests/decorators_tests.py
@@ -4,7 +4,7 @@ from transact.job import Job
 
 def test_no_undo_operation_decorator():
 
-    @no_undo_operation
+    @no_undo_operation()
     def test_func(test):
         assert test == "test"
         return True
@@ -16,7 +16,7 @@ def test_no_undo_operation_decorator():
 
 def test_no_do_operation_decorator():
 
-    @no_do_operation
+    @no_do_operation()
     def test_func(test):
         assert test == "test"
         return True
@@ -30,7 +30,7 @@ def test_no_undo_operation_decorator_job_attribute():
 
     job = Job(test="test")
 
-    @no_undo_operation
+    @no_undo_operation()
     def test_func(test, job):
         assert test == "test"
         assert isinstance(job, Job)
@@ -44,7 +44,7 @@ def test_no_do_operation_decorator_job_attribute():
 
     job = Job(test="test")
 
-    @no_do_operation
+    @no_do_operation()
     def test_func(test, job):
         assert test == "test"
         assert isinstance(job, Job)
@@ -52,3 +52,33 @@ def test_no_do_operation_decorator_job_attribute():
         return True
 
     test_func.undo(job)
+
+
+def test_no_undo_operation_decorator_retry():
+
+    job = Job(counter=0)
+    @no_undo_operation(3)
+    def test_func(job):
+        job.counter += 1
+        if job.counter < 3:
+            return False
+        return True
+
+    test_func.do(job)
+
+    assert job.counter == 3
+    assert not job.failure
+
+
+def test_no_undo_operation_decorator_retry_failure():
+    job = Job(counter=0)
+
+    @no_undo_operation(2)
+    def test_func(job):
+        job.counter += 1
+        return False
+
+    test_func.do(job)
+
+    assert job.counter == 3
+
--- transact/decorators.py
+++ transact/decorators.py
@@ -1,43 +1,47 @@
 from typing import Callable, Any
 from logging import warning
+from inspect import signature
 
 from transact.operation import NoUndoOperation, NoDoOperation
 from transact.job import Job
 
 
-def no_undo_operation(func: Callable[[Any], bool]) -> NoUndoOperation:
+def no_undo_operation(num_retry: int = 0) -> Callable[[Callable[[Any], bool]], NoUndoOperation]:
+    def _decorator(func: Callable[[Any], bool]) -> NoUndoOperation:
 
-    def do_action(job: Job):
-        kwargs = {}
-        for key in func.__code__.co_varnames:
-            if key == "job":
-                kwargs[key] = job
-                continue
+        def do_action(job: Job):
+            kwargs = {}
+            for key in signature(func).parameters:
+                if key == "job":
+                    kwargs[key] = job
+                    continue
 
-            try:
-                kwargs[key] = job.__getattribute__(key)
-            except AttributeError:
-                warning(f'Ignoring attribute {key} because not in job {job}')
+                try:
+                    kwargs[key] = job.__getattribute__(key)
+                except AttributeError:
+                    warning(f'Ignoring attribute {key} because not in job {job}')
 
-        return func(**kwargs)
+            return func(**kwargs)
 
-    return NoUndoOperation(do_action)
+        return NoUndoOperation(do_action, retry=num_retry)
+    return _decorator
 
 
-def no_do_operation(func: Callable[[Any], bool]) -> NoDoOperation:
+def no_do_operation(num_retry: int = 0) -> Callable[[Callable[[Any], bool]], NoDoOperation]:
+    def _decorator(func: Callable[[Any], bool]) -> NoDoOperation:
+        def undo_action(job: Job):
+            kwargs = {}
+            for key in signature(func).parameters:
+                if key == "job":
+                    kwargs[key] = job
+                    continue
 
-    def undo_action(job: Job):
-        kwargs = {}
-        for key in func.__code__.co_varnames:
-            if key == "job":
-                kwargs[key] = job
-                continue
+                try:
+                    kwargs[key] = job.__getattribute__(key)
+                except AttributeError:
+                    warning(f'Ignoring attribute {key} because not in job {job}')
 
-            try:
-                kwargs[key] = job.__getattribute__(key)
-            except AttributeError:
-                warning(f'Ignoring attribute {key} because not in job {job}')
+            return func(**kwargs)
 
-        return func(**kwargs)
-
-    return NoDoOperation(undo_action)
+        return NoDoOperation(undo_action, retry=num_retry)
+    return _decorator
